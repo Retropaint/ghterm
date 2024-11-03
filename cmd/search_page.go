58d93cc73830ecfd90c2b6856a321b17e30611a7
Retropaint <70713407+Retropaint@users.noreply.github.com>
2024-11-03 21:41:29 +0700

    input placeholder & input capture

--- cmd/search_page.go
+++ cmd/search_page.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"sync"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
@@ -28,37 +27,45 @@ type SearchPage struct {
 	input *tview.InputField
 	list  *tview.TextView
 
-	result SearchResult
-	wg     sync.WaitGroup
+	result    SearchResult
+	repoIndex int
 }
 
 func (sp *SearchPage) Init() {
 	sp.input = tview.NewInputField()
-	sp.input.SetFieldBackgroundColor(tcell.ColorBlack)
 	sp.input.SetBorder(true)
 	sp.input.SetDoneFunc(sp.search)
+	sp.input.SetPlaceholder("search something...")
+	sp.input.SetPlaceholderStyle(sp.input.GetPlaceholderStyle().
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorGrey),
+	)
+	sp.input.SetFieldBackgroundColor(tcell.ColorBlack)
 
 	sp.list = tview.NewTextView()
 	sp.list.SetBorder(true)
 	sp.list.SetRegions(true)
 	sp.list.SetDynamicColors(true)
+	sp.list.SetTitle("Results")
+	sp.list.SetTitleAlign(tview.AlignLeft)
 
 	sp.Flex = tview.NewFlex().
 		SetDirection(tview.FlexRow).
 		AddItem(sp.input, 3, 0, true).
 		AddItem(sp.list, 0, 3, false)
 
-	sp.clearList("Use the search above to find a repository!")
 	sp.Flex.SetInputCapture(sp.onInputCapture)
 }
 
 func (sp *SearchPage) search(key tcell.Key) {
-	sp.clearList("loading...")
+	sp.clearList("[lightgrey]loading...[-]")
 	go func() {
-		sp.wg.Add(1)
 		sp.fetch(&sp.result, "https://api.github.com/search/repositories?q="+sp.input.GetText()+"&per_page=3")
 		sp.clearList("")
 		sp.populateList()
+		sp.repoIndex = 0
+		sp.highlightResult()
+		Layout.App.Draw()
 	}()
 }
 
@@ -72,8 +79,28 @@ func (sp *SearchPage) onInputCapture(event *tcell.EventKey) *tcell.EventKey {
 		Layout.App.SetFocus(sp.list)
 	case "Ctrl+F":
 		Layout.App.SetFocus(sp.input)
+	default:
+		sp.listOnInputCapture(event)
 	}
 	return event
+}
+
+func (sp *SearchPage) listOnInputCapture(event *tcell.EventKey) {
+	if Layout.App.GetFocus() != sp.list {
+		return
+	}
+	switch event.Name() {
+	case "Rune[j]":
+		sp.repoIndex = min(len(sp.result.Items)-1, sp.repoIndex+1)
+	case "Rune[k]":
+		sp.repoIndex = max(0, sp.repoIndex-1)
+	}
+	sp.highlightResult()
+}
+
+func (sp *SearchPage) highlightResult() {
+	region := strings.Replace(sp.result.Items[sp.repoIndex].Full_name, "/", ".", 1)
+	sp.list.Highlight(region)
 }
 
 func (sp *SearchPage) populateList() {
@@ -86,7 +113,6 @@ func (sp *SearchPage) populateList() {
 }
 
 func (sp *SearchPage) fetch(obj any, url string) {
-	defer sp.wg.Done()
 	client := &http.Client{}
 
 	request, err := http.NewRequest("GET", url, nil)
